f0c31df48489c05f6a0cc0901c9da8ee9ceb09ce
Mia Barbir <miabarbir13@gmail.com>
2019-11-06 09:24:45 +0100

    structure

---
+++ model/config.php
@@ -0,0 +1,25 @@
+<?php
+
+function openConnection() {
+	// Try to figure out what these should be for you
+	$dbhost = "remotemysql.com";
+	$dbuser = "7c1hcMepnL";
+	$dbpass = "6e7ns21xdW";
+	$db = "7c1hcMepnL";
+
+	// Try to understand what happens here
+	$pdo = new PDO("mysql:host=$dbhost;dbname=$db",$dbuser,$dbpass);
+
+	// Why we do this here
+	return $pdo;
+}
+
+try{
+    openConnection();
+    echo "Connected";
+    
+} catch(Exception $e){
+    echo "Error". $e -> getMessage();
+    die();
+}
+?>
