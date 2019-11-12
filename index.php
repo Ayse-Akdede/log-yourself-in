5d07727c118aa9d8ab2958dc7d890b90948ef496
Mia Barbir <miabarbir13@gmail.com>
2019-11-12 11:20:49 +0100

    assets

--- index.php
+++ index.php
@@ -65,49 +65,51 @@ if(isset($error)) {
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Hackers Poulette</title>
+    <title>Sign up</title>
+    <link rel="stylesheet" type="text/css" href="assets/css/style.css">
+    <link href="https://fonts.googleapis.com/css?family=Open+Sans|Roboto&display=swap" rel="stylesheet">
 </head>
 <body>
     <div align="center">
         <h1>Sign up</h1>
         <form action="index.php" method="post">
-            <table>
-                <tr>
-                    <td>
-                        <label for="username">
-                            Username : 
-                        </label>
-                    </td>
-                    <td>
-                        <input type="text" placeholder="Your username" id="username" name="username" alt="username" value="<?php if(isset($username)) { echo $username; } ?>">
-                    </td>
-                </tr>
-                <tr>
-                    <td>
-                        <label for="email">
-                            E-mail : 
-                        </label>
-                    </td>
-                    <td>
-                        <input type="email" placeholder="Your e-mail" id="email" name="email" alt="email" value="<?php if(isset($email)) { echo $email; } ?>">
-                    </td>
-                </tr>
-                <tr>
-                    <td>
-                        <label for="password">
-                            Password : 
-                        </label>
-                    </td>
-                    <td>
-                        <input type="password" placeholder="Your password" id="password" name="password" alt="password">
-                    </td>
-                </tr>      
-            </table>
-            <br>
-            <input type="submit" name="formregistration" value="Sign Up">
+            <div class="form-style">
+                <table>
+                    <tr>
+                        <td>
+                            <label for="username">
+                                Username : 
+                            </label>
+                        </td>
+                        <td>
+                            <input type="text" placeholder="Your username" id="username" name="username" alt="username" value="<?php if(isset($username)) { echo $username; } ?>">
+                        </td>
+                    </tr>
+                    <tr>
+                        <td>
+                            <label for="email">
+                                E-mail : 
+                            </label>
+                        </td>
+                        <td>
+                            <input type="email" placeholder="Your e-mail" id="email" name="email" alt="email" value="<?php if(isset($email)) { echo $email; } ?>">
+                        </td>
+                    </tr>
+                    <tr>
+                        <td>
+                            <label for="password">
+                                Password : 
+                            </label>
+                        </td>
+                        <td>
+                            <input type="password" placeholder="Your password" id="password" name="password" alt="password">
+                        </td>
+                    </tr>      
+                </table>
+                <br>
+                <input type="submit" name="formregistration" value="Sign Up">
+            </div>
         </form>
-
-        
     </div>
 </body>
 </html>
